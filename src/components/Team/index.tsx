e6012335991352aa7c6ee7e778a2e5a984704e7d
kopichris <chrisjd@nus.edu.sg>
2025-02-17 11:42:06 +0800

    feat: create sections for team page

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -8,94 +8,85 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import styles from './styles.module.css';
+import { CardActionArea } from '@mui/material';
 
 type TeamMember = {
   name: string;
-  github: string;
-  scholar: string;
   position: string;
   image: string;
 };
 
-const team: TeamMember[] = [
+let hero = "/img/hero.png";
+
+const facilityHead: TeamMember[] = [
   {
-    name: "Person 1",
-    github: "https://github.com/alicejohnson",
-    scholar: "https://scholar.google.com/citations?user=alice123",
-    position: "Prinicpal Investigator",
-    image: "",
-  },
-  {
-    name: "Person 2",
-    github: "https://github.com/bsmith",
-    scholar: "https://scholar.google.com/citations?user=bob456",
-    position: "Research Associate Professor",
-    image: "",
-  },
-  {
-    name: "Person 3",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Senior Research Scientist",
-    image: "",
-  },
-  {
-    name: "Person 4",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Senior Research Scientist",
-    image: "",
-  },
-  {
-    name: "Person 5",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "IT Manager",
-    image: "",
-  },
-  {
-    name: "Person 6",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Research Associate",
-    image: "",
-  },
-  {
-    name: "Person 7",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Software Engineer",
-    image: "",
-  },
-  {
-    name: "Person 8",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Research Fellow",
-    image: "",
-  },
-  {
-    name: "Person 9",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "Research Fellow",
-    image: "",
-  },
-  {
-    name: "Person 10",
-    github: "https://github.com/catlee",
-    scholar: "https://scholar.google.com/citations?user=cat789",
-    position: "IT Analyst",
-    image: "",
+    name: "Jason Pitt",
+    position: "Principal Investigator",
+    image: hero,
   },
 ];
 
-const TeamCard: React.FC<TeamMember> = ({ name, github, scholar, position }) => {
+const serviceLeads: TeamMember[] = [
+  {
+    name: "Henry Yang",
+    position: "Associate Professor",
+    image: hero,
+  },
+  {
+    name: "Kumaran Mande",
+    position: "Senior Research Scientist",
+    image: hero,
+  },
+  {
+    name: "Tony Tan",
+    position: "Senior Research Scientist",
+    image: hero,
+  },
+  {
+    name: "CK Ong",
+    position: "IT Manager",
+    image: hero,
+  },
+];
+
+const teamMembers: TeamMember[] = [
+  {
+    name: "Li Ying",
+    position: "IT Manager",
+    image: hero,
+  },
+  {
+    name: "Linganesan Mani",
+    position: "Software Engineer",
+    image: hero,
+  },
+  {
+    name: "Chris Dean",
+    position: "Research Fellow",
+    image: hero,
+  },
+  {
+    name: "Hannan Wong",
+    position: "Research Fellow",
+    image: hero,
+  },
+  {
+    name: "Chia Y. Keng",
+    position: "IT Analyst",
+    image: hero,
+  },
+];
+
+const TeamCard: React.FC<TeamMember> = ({ name, position, image }) => {
   return(
-      <Card sx={{ maxWidth: 200 }}>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Card sx={{ width: 200, height: 250, display: 'flex', flexDirection: 'column' }}>
+        <CardMedia 
+          sx={{ height: 200 }}
+          image={image}
+          title={name}
+        />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h6" component="div">
             { name }
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
@@ -108,10 +99,33 @@ const TeamCard: React.FC<TeamMember> = ({ name, github, scholar, position }) => 
 
 const TeamPagePeople: React.FC = () => {
   return (
-    <div className={styles.flexContainer}>
-      {team.map((member) => (
-        <TeamCard key={member.name} {...member} />
-      ))}
+    <div className={styles.teamContainer}>
+      <section className={styles.teamSection}>
+        <h1 className={styles.sectionTitle}>Facility Head</h1>
+        <div className={styles.teamGrid}>
+          {facilityHead.map((member) => (
+            <TeamCard key={member.name} {...member}/>
+          ))}
+        </div>
+      </section>
+
+      <section className={styles.teamSection}>
+        <h1 className={styles.sectionTitle}>Service Leads</h1>
+        <div className={styles.teamGrid}>
+          {serviceLeads.map((member) => (
+            <TeamCard key={member.name} {...member}/>
+          ))}
+        </div>
+      </section>
+
+      <section className={styles.teamSection}>
+        <h1 className={styles.sectionTitle}>Team Members</h1>
+        <div className={styles.teamGrid}>
+          {teamMembers.map((member) => (
+            <TeamCard key={member.name} {...member}/>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
