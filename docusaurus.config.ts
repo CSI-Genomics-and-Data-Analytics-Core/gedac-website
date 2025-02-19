f2b21bff0d8e38612799ecbd17dd80ab72f34675
kopichris <chrisjd@nus.edu.sg>
2025-02-19 11:51:59 +0800

    feat: change to docs-only mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,6 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/CSI-Genomics-and-Data-Analytics-Core/gedac-website',
+          routeBasePath: '/',
         },
         theme: {
           customCss: './src/css/custom.css',
